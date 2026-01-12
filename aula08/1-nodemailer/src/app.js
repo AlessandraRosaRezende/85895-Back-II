8527ef48d46b20fd2243832bbdf4da75b9929213
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2026-01-12 20:10:44 -0300

    feat: nodemailer

---
+++ aula08/1-nodemailer/src/app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  service: 'Gmail',
+  port: 587, // porta default do Gmail
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD
+  },
+});
+
+app.get('/', (req, res) => {
+  return res.send('Hello World!');
+})
+
+app.get('/mail', async (req, res) => {
+  const result = await transport.sendMail({
+    from: `Coder Tests <${process.env.GMAIL_USER}>`,
+    to: `endereço para onde o email será enviado <${process.env.GMAIL_USER}>`,
+    subject: 'Test email with Node.js and Nodemailer',
+    html: `
+      <div>
+        <h1 style="color: blue;">This is a test email</h1>
+        <p>This email was sent using <b>Node.js</b> and <b>Nodemailer</b>!</p>
+      </div>
+    `,
+    attachments: []
+  });
+  return res.json({ message: 'Email sent', result });
+});
+
+app.listen(8080, () => {
+  console.log('Server running on http://localhost:8080');
+});
